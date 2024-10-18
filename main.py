4c1e3321e5484cb4c28268211f1331f6d5b637aa
StrideMB <153535246+StrideMB@users.noreply.github.com>
2024-10-18 19:53:43 +0800

    Add Runner class

--- main.py
+++ main.py
@@ -7,63 +7,60 @@ from Database import Database
 from Llms import llm
 
 
+class Runner:
+    def __init__(self):
+        self.embedding_tokenizer = None
+        self.embedding_model = None
+        self.llm_tokenizer = None
+        self.llm = None
 
-def init_models():
-    print("Loading Embedding Model...")
-    tokenizer = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_PATH)
-    embedding_model = AutoModel.from_pretrained(EMBEDDING_MODEL_PATH)
+    def init_models(self):
+        print("Loading Embedding Model...")
+        self.embedding_tokenizer = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_PATH)
+        self.embedding_model = AutoModel.from_pretrained(EMBEDDING_MODEL_PATH)
 
-    print("Loading LLM...")
-    llm_tokenizer = AutoTokenizer.from_pretrained(LLM_PATH)
-    llm = Qwen2ForCausalLM.from_pretrained(LLM_PATH)
+        print("Loading LLM...")
+        self.llm_tokenizer = AutoTokenizer.from_pretrained(LLM_PATH)
+        self.llm = Qwen2ForCausalLM.from_pretrained(LLM_PATH)
     
-    print("Models loaded.")
-    return tokenizer, embedding_model, llm_tokenizer, llm
+        print("Models loaded.")
 
-
-def init_database(tokenizer, embedding_model):
-    database = Database(DATABASE_NAME, tokenizer, embedding_model)
+    def init_database(self):
+        self.database = Database(DATABASE_NAME, self.embedding_tokenizer, self.embedding_model)
     
-    FILES = os.listdir(FILES_PATH)
-    files = FILES.copy()
+        FILES = os.listdir(FILES_PATH)
+        files = FILES.copy()
 
-    for file in database.files:
-        if file not in files:
-            files.append(file)
+        for file in self.database.files:
+            if file not in files:
+                files.append(file)
     
-    for file in files:
-        if file in database.files and file in FILES:
-            print(f"{file} already in {database.db}.")
-            continue
-        elif file in database.files:
-            database.delete(file)
-            print(f"{file} deleted from {database.db}.")
-        else:
-            database.add(file)
-            print(f"{file} added to {database.db}.")
+        for file in files:
+            if file in self.database.files and file in FILES:
+                print(f"{file} already in {self.database.db}.")
+                continue
+            elif file in self.database.files:
+                self.database.delete(file)
+                print(f"{file} deleted from {self.database.db}.")
+            else:
+                self.database.add(file)
+                print(f"{file} added to {self.database.db}.")
     
-    print(f"{database.db} initialized.")
-    return database
+        print(f"{self.database.db} initialized.")
 
-
-def ask_ai(database, llm_tokenizer, llmodel):
-    question = input("我是AI课程助手，有什么可以帮你？\n")
-    while True:
-        top_k = database.top_k_chunks(question)
-        answer = llm(llm_tokenizer, llmodel, question, top_k)
-        print(answer)
-        question = input()
-
+    def ask_ai(self):
+        question = input("我是AI课程助手，有什么可以帮你？\n")
+        while True:
+            top_k = self.database.top_k_chunks(question)
+            answer = llm(self.llm_tokenizer, self.llm, question, top_k)
+            print(answer)
+            question = input()
 
 def main():
-    embedding_tokenizer, embedding_model, llm_tokenier, llm = init_models()
-    
-    database = init_database(embedding_tokenizer, embedding_model)
-    
-    ask_ai(database, llm_tokenier, llm)
-
+    runner = Runner()
+    runner.init_models()
+    runner.init_database()
+    runner.ask_ai()
 
 if __name__ == "__main__":
     main()
-
-
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,65 @@
+
+
+from config import EMBEDDING_MODEL_PATH, LLM_PATH, FILES_PATH, DATABASE_NAME
+from Database import Database
+from Llms import llm
+
+
+
+def init_models():
+    print("Loading Embedding Model...")
+    tokenizer = AutoTokenizer.from_pretrained(EMBEDDING_MODEL_PATH)
+    embedding_model = AutoModel.from_pretrained(EMBEDDING_MODEL_PATH)
+
+    print("Loading LLM...")
+    llm_tokenizer = AutoTokenizer.from_pretrained(LLM_PATH)
+    llm = Qwen2ForCausalLM.from_pretrained(LLM_PATH)
+    
+    print("Models loaded.")
+    return tokenizer, embedding_model, llm_tokenizer, llm
+
+
+def init_database(tokenizer, embedding_model):
+    database = Database(DATABASE_NAME, tokenizer, embedding_model)
+    
+    FILES = os.listdir(FILES_PATH)
+    files = FILES.copy()
+
+    for file in database.files:
+        if file not in files:
+            files.append(file)
+    
+    for file in files:
+        if file in database.files and file in FILES:
+            print(f"{file} already in {database.db}.")
+            continue
+        elif file in database.files:
+            database.delete(file)
+            print(f"{file} deleted from {database.db}.")
+        else:
+            database.add(file)
+            print(f"{file} added to {database.db}.")
+    
+    print(f"{database.db} initialized.")
+    return database
+
+
+def ask_ai(database, llm_tokenizer, llmodel):
+    question = input("我是AI课程助手，有什么可以帮你？\n")
+    while True:
+        top_k = database.top_k_chunks(question)
+        answer = llm(llm_tokenizer, llmodel, question, top_k)
+        print(answer)
+        question = input()
+
+
+def main():
+    embedding_tokenizer, embedding_model, llm_tokenier, llm = init_models()
+    
+    database = init_database(embedding_tokenizer, embedding_model)
+    
+    ask_ai(database, llm_tokenier, llm)
+
+
+if __name__ == "__main__":
+    main()
